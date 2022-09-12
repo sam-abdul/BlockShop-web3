51caf86f691132750461ed8ffcd5da7c5e73a3c8
Sam Abdul <72390202+sam-abdul@users.noreply.github.com>
2022-09-12 20:52:24 +0100

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,14 +1,14 @@
-import Link from 'next/link'
-import { useState } from 'react'
-import { useMoralis } from 'react-moralis'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSearch, faUser} from '@fortawesome/free-solid-svg-icons'
-import { useSelector } from 'react-redux';
-import Logo from '../public/asset/blockshop.png';
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Link from "next/link";
+import { useState } from "react";
+import { useMoralis } from "react-moralis";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch, faUser } from "@fortawesome/free-solid-svg-icons";
+import { useSelector } from "react-redux";
+import Logo from "../public/asset/blockshop.png";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
 export const Navbar = () => {
-  const [active, setActive] = useState(false)
+  const [active, setActive] = useState(false);
 
   const cart = useSelector((state) => state.cart);
 
@@ -18,15 +18,16 @@ export const Navbar = () => {
   };
 
   const handleClick = () => {
-    setActive(!active)
-  }
-  const { authenticate, isAuthenticated, logout, account, user, authError } = useMoralis()
+    setActive(!active);
+  };
+  const { authenticate, isAuthenticated, logout, account, user, authError } =
+    useMoralis();
   return (
     <>
       <nav className="flex flex-wrap items-c  enter bg-black p-3 ">
         <Link href="/">
           <a className="mr-4 inline-flex items-center p-2 ">
-           <Image src={Logo} height={30} width={30}/>
+            <Image src={Logo} height={30} width={30} />
             <span className="text-xl font-bold tracking-wide text-white">
               BlockShop
             </span>
@@ -54,24 +55,25 @@ export const Navbar = () => {
         {/*Note that in this div we will use a ternary operator to decide whether or not to display the content of the div  */}
         <div
           className={`${
-            active ? '' : 'hidden'
+            active ? "" : "hidden"
           }   w-full lg:inline-flex lg:w-auto lg:flex-grow`}
         >
           <div className="flex w-full flex-col items-start lg:ml-auto lg:inline-flex lg:h-auto  lg:w-auto lg:flex-row lg:items-center">
-          <Link href="/search">
+            <Link href="/search">
               <a className="w-full items-center justify-center rounded px-3 py-2 font-bold text-white hover:bg-blue-400 hover:text-white lg:inline-flex lg:w-auto">
-              <FontAwesomeIcon
+                <FontAwesomeIcon
                   icon={faSearch}
-                  style={{ fontSize: 20, color: 'white' }}
+                  style={{ fontSize: 20, color: "white" }}
                 />
               </a>
             </Link>
-          <Link href="/user">
+            <Link href="/user">
               <a className="w-full items-center justify-center rounded px-3 py-2 font-bold text-white hover:bg-blue-400 hover:text-white lg:inline-flex lg:w-auto">
                 <FontAwesomeIcon
                   icon={faUser}
-                  style={{ fontSize: 20, color: 'white' }}
-                />({getItemsCount()})
+                  style={{ fontSize: 20, color: "white" }}
+                />
+                ({getItemsCount()})
               </a>
             </Link>
             <Link href="/">
@@ -88,18 +90,22 @@ export const Navbar = () => {
                   <button
                     className="w-full font-bold"
                     onClick={() => {
-                      authenticate({ provider: 'metamask' })
+                      authenticate({ provider: "metamask" });
                     }}
                   >
-                   {account? <span>{account.slice(0,5)}...</span> : <span>Connect Wallet</span> }  
+                    {account ? (
+                      <span>{account.slice(0, 5)}...</span>
+                    ) : (
+                      <span>Connect Wallet</span>
+                    )}
                   </button>
-
-                  
                 )}
-                {authError && <div className={styles.error}>
-                  <span>{authError.name}</span>
-                  {authError.message}
-                  </div>}
+                {authError && (
+                  <div className={styles.error}>
+                    <span>{authError.name}</span>
+                    {authError.message}
+                  </div>
+                )}
               </a>
             </Link>
             {/* <Link href="/contact">
@@ -111,5 +117,5 @@ export const Navbar = () => {
         </div>
       </nav>
     </>
-  )
-}
+  );
+};
